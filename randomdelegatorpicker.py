aadbf8a64ac729f89ad815edd33a197d0d73b9f4
glagolef <glagolef@tcd.ie>
2021-07-29 21:14:10 +0200

    sqrt init

--- randomdelegatorpicker.py
+++ randomdelegatorpicker.py
@@ -1,13 +1,11 @@
 #!/bin/env python3
 import json
 import argparse
+import math
 import random
 import subprocess
 
 from os import path
-
-# Global var
-try_bech32 = True
 
 
 def parse_all_args():
@@ -54,6 +52,12 @@ def parse_all_args():
         help="if used, the winners will be unique (max 1 prize per address). "
              + "Only makes sense to use if --winners is specified."
     )
+    parser.add_argument(
+        '-s', "--sqrt",
+        action="store_true",
+        help="if used, the participants' number of tickets will be square rooted, "
+             + "giving smaller guys a greater chance of winning."
+    )
     return parser.parse_args()
 
 
@@ -70,25 +74,26 @@ def maybe_run_bech32(addr):
     return addr
 
 
-def process_winner(winning_num, n, _total_tickets):
+def process_winner(winning_num, prize_num, _total_tickets):
     accum = 0
-    print("Prize #" + str(n) + " Winning number: " + str(winning_num))
-    for p in eligible_participants:
-        participant_tickets = eligible_participants[p]
+    eligible_participants_length = len(eligible_participants)
+    print("Prize #" + str(prize_num) + " Winning number: " + str(winning_num))
+    for participant in eligible_participants:
+        participant_tickets = eligible_participants[participant]
         if participant_tickets == 0:
             continue
         accum += participant_tickets
-        if accum > winning_num and participant_tickets > 0:
-            winner = maybe_run_bech32(p)
+        if round(accum) >= winning_num and participant_tickets > 0:
+            winner = maybe_run_bech32(participant)
             print_result(winner, participant_tickets, _total_tickets)
             if unique:
-                eligible_participants[p] = 0
+                eligible_participants[participant] = 0
                 _total_tickets -= participant_tickets
             break
-    if (accum < winning_num):
-        print("!!! Something probably went wrong. accum < winning_num. " + ": accum =" + str(
-            accum) + ", winning_num = " + str(winning_num))
-    return _total_tickets
+    if round(accum) < winning_num:
+        print("!!! Something probably went wrong. accum < winning_num. " + ": accum =" + str(accum)
+              + ", winning_num = " + str(winning_num))
+    return round(_total_tickets)
 
 
 def calculate_chance(tickets, total_tickets):
@@ -96,14 +101,19 @@ def calculate_chance(tickets, total_tickets):
 
 
 def print_result(winner, tickets, total_tickets):
-    if giveaway_type == 1:
-        congrats = "Congrats to " + winner + " (" + str(round(tickets / million)) + " out of " \
-                   + str(round(total_tickets / million)) + " ADA, " \
-                   + (calculate_chance(tickets, total_tickets)) + "% chance)!\n"
+    tickets = round(tickets)
+    if giveaway_type == delegator_str and not use_sqrt:
+        divisor = million
     else:
-        congrats = "Congrats to " + winner + " (" + str(tickets) + " out of " + \
-                   str(total_tickets) + " tokens, " + (calculate_chance(tickets, total_tickets)) + "% chance)!\n"
+        divisor = 1
+    congrats = get_congrats_message(winner, tickets, total_tickets, divisor)
     print(congrats)
+
+
+def get_congrats_message(winner, tickets, total_tickets, divisor):
+    return "Congrats to " + winner + " (" + str(round(tickets / divisor)) + " out of " \
+           + str(round(total_tickets / divisor)) + " tickets, " \
+           + (calculate_chance(tickets, total_tickets)) + "% chance)!\n"
 
 
 def get_min_tokens():
@@ -112,9 +122,17 @@ def get_min_tokens():
     else:
         _min_tokens = abs(int(min_tokens_arg))
 
-    if giveaway_type == 1:
+    if giveaway_type == delegator_str:
         _min_tokens *= million
     return _min_tokens
+
+
+def maybe_apply_sqrt(num):
+    if use_sqrt:
+        return math.sqrt(num)
+        # if more than 1 winners, do sqrt operation when collecting eligible participants
+    else:
+        return num
 
 
 # Parsing Args
@@ -127,10 +145,15 @@ exclude_addresses = args.exclude_addresses
 number_winners_arg = args.number_winners
 min_tokens_arg = args.min_tokens
 unique = args.unique
+use_sqrt = args.sqrt
 
 million = 1000000
+delegator_str = "delegator"
+token_hodler_str = "token_hodler"
 
-giveaway_type = 0
+giveaway_type = ""
+try_bech32 = False
+
 if poolId is None and policyId is None:
     print("Neither --pool-id nor --policy-id was specified!")
     exit()
@@ -138,32 +161,44 @@ elif poolId is not None and policyId is not None:
     print("--pool-id and --policy-id arguments are not supported, please use only one of them.")
     exit()
 elif poolId is not None:
-    giveaway_type = 1
-    try_bech32 = False
+    giveaway_type = delegator_str
 elif policyId is not None:
-    giveaway_type = 2
+    giveaway_type = token_hodler_str
+    try_bech32 = True
 
 # TODO:implement --exclude for token giveaways
-if giveaway_type == 2 and exclude_addresses is not None:
+if giveaway_type == token_hodler_str and exclude_addresses is not None:
     print("--exclude ADDRESSES param is not yet supported for token giveaways and will be ignored.")
 
 min_tokens = get_min_tokens()
 
 ledger = args.ledger
 
+if number_winners_arg is not None:
+    number_winners = abs(int(number_winners_arg))
+else:
+    number_winners = 1
+
+problems = 0
+
 if not path.exists(ledger):
     print("We tried but could not locate your ledger-state JSON file!")
     print("Use: \033[1;34mcardano-cli query ledger-state --mainnet --out-file ledger.json\033[0m to export one!")
     exit()
 
-with open(ledger) as f:
-    ledger = json.load(f)
+with open(ledger) as ledger_stream:
+    ledger = json.load(ledger_stream)
 
 eligible_participants_total = 0
-tickets_total = 0
 eligible_participants = {}
 
-if giveaway_type == 1:
+ineligible_participants_total = 0
+eligible_tokens_total = 0
+ineligible_tokens_total = 0
+
+sqrt_eligible_tokens_total = 0
+
+if giveaway_type == delegator_str:
     stakequery = "pstakeSet"
     stakeinfo = "active"
 
@@ -217,77 +252,78 @@ if giveaway_type == 1:
             else:
                 blockstake[delegatorid] = blockstake[delegatorid] + snapstake
     total_bs = 0
-    excluded_stake = 0
-    for d in blockstakedelegators[poolId]:
-        if d in blockstake:
-            activestake = blockstake[d]
-            if exclude_addresses is not None and exclude_addresses.__contains__(d):
-                excluded_stake += activestake
-            elif activestake > min_tokens:
-                tickets_total += activestake
-                eligible_participants_total += 1
-                eligible_participants[d] = activestake
+    for delegator in blockstakedelegators[poolId]:
+        if delegator in blockstake:
+            activestake = blockstake[delegator]
+            if exclude_addresses.__contains__(delegator) or min_tokens > activestake:
+                ineligible_tokens_total += activestake
+                ineligible_participants_total += 1
             else:
-                excluded_stake += activestake
+                eligible_tokens_total += activestake
+                activestake = maybe_apply_sqrt(activestake)
+                sqrt_eligible_tokens_total += activestake
+                eligible_participants[delegator] = activestake
+    eligible_participants_total = len(eligible_participants)
     print("Total pool stake on record: " + str(totalRecordedActiveStake))
-    print("Total calculated pool stake: " + str((tickets_total + excluded_stake) / million) + "\n")
-    print("Total eligible stake: " + str(tickets_total / million))
-    print("Number of eligible addresses: " + str(eligible_participants_total))
+    print("Total calculated pool stake (ADA): " + str((eligible_tokens_total + ineligible_tokens_total) / million) + "\n")
+    print("Total # of eligible addresses: " + str(eligible_participants_total))
+    print("Total eligible stake (ADA): " + str(eligible_tokens_total / million))
 
-elif giveaway_type == 2:
+elif giveaway_type == token_hodler_str:
     utxos = ledger["stateBefore"]['esLState']['utxoState']['utxo']
-    policy_holders = {}
-    total_tokens = 0
-    total_token_holders = 0
-    total_eligible_tokens = 0
     for utxo in utxos:
         numberOfTokens = 0
         for policy in utxos[utxo]['amount']['policies']:
             if policy == policyId:
                 address = utxos[utxo]['address']
                 ph_tokens = 0
-                total_token_holders += 1
                 for x in utxos[utxo]['amount']['policies'][policyId]:
                     ph_tokens += (utxos[utxo]['amount']['policies'][policyId][x])
                 if ph_tokens > min_tokens:
-                    policy_holder_address = policy_holders.get(address)
+                    policy_holder_address = eligible_participants.get(address)
+                    eligible_tokens_total += ph_tokens
+                    ph_tokens = maybe_apply_sqrt(ph_tokens)
+                    sqrt_eligible_tokens_total += ph_tokens
                     if policy_holder_address is not None and policy_holder_address > 0:
-                        policy_holders[address] += ph_tokens
-                        total_token_holders -= 1
+                        eligible_participants[address] += ph_tokens
+                        # discount multiple entries by same address
+                        eligible_participants_total -= 1
                     else:
-                        policy_holders[address] = ph_tokens
-                    total_eligible_tokens += ph_tokens
-                total_tokens += ph_tokens
-    tickets_total = total_eligible_tokens
-    eligible_participants = policy_holders
-    eligible_participants_total = len(policy_holders)
+                        eligible_participants[address] = ph_tokens
+                else:
+                    ph_tokens = maybe_apply_sqrt(ph_tokens)
+                    ineligible_tokens_total += ph_tokens
+                    ineligible_participants_total += 1
 
-    print("Total # token holders: " + str(total_token_holders))
-    print("Total # tokens minted: " + str(total_tokens))
+    eligible_participants_total = len(eligible_participants)
+
+    print("Total # token holders: " + str(ineligible_participants_total + eligible_participants_total))
+    print("Total # tokens minted: " + str(eligible_tokens_total + ineligible_tokens_total))
     print("Total # eligible token holders: " + str(eligible_participants_total))
-    print("Total # eligible tokens: " + str(total_eligible_tokens))
+    print("Total # eligible tokens: " + str(eligible_tokens_total))
 
-f.close()
+if use_sqrt:
+    print("Total eligible tickets: " + str(sqrt_eligible_tokens_total))
+    eligible_tokens_total = sqrt_eligible_tokens_total
+
+ledger_stream.close()
 
 # print("Full list of eligible participants:\n" + str(participants))
 print()
 
-if number_winners_arg is not None:
-    number_winners = abs(int(number_winners_arg))
-    problems = 0
-    if unique and eligible_participants_total < number_winners:
-        exit("Too few delegators to pick from. Try a lower number of winners or omit --unique flag")
-    for i in range(number_winners):
-        try:
-            winning_num = random.randint(1, tickets_total)
-            tickets_total = process_winner(winning_num, i + 1, tickets_total)
-        except:
-            problems += 1
-    if problems > 0:
-        print("A number of problems occurred:" + str(problems))
-else:
-    winning_num = random.randint(1, tickets_total)
-    tickets_total = process_winner(winning_num, 1, tickets_total)
-if not try_bech32 and giveaway_type == 2:
+# if number_winners > 1:
+if unique and eligible_participants_total < number_winners:
+    exit("Too few delegators to pick from. Try a lower number of winners or omit --unique flag")
+tickets_total = round(eligible_tokens_total)
+for prize_num in range(number_winners):
+    try:
+        winning_num = random.randint(1, tickets_total)
+        tickets_total = process_winner(winning_num, prize_num + 1, tickets_total)
+    except:
+        problems += 1
+if problems > 0:
+    print("A number of problems occurred:" + str(problems))
+
+if not try_bech32 and giveaway_type == token_hodler_str:
     print("You may need to use bech32 to convert winning addresses to 'addr...' format.\n")
 print("Done! Well done to the winners, best of luck next time to everyone else!")
--- runbech32.sh
+++ runbech32.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 #specify bech32 path below if it's not in $PATH
-#bech32=""
+#bech32="/path/to/bech32"
 if command -v bech32 addr $1 > /dev/null 2>&1
 then
-  echo $1 | bech32 addr
+  if [[ $1 == addr* ]]
+  then
+    echo $1 | bech32
+  else
+    echo $1 | bech32 addr
+  fi
 else
   echo $1
 fi
